ba7c18b741840106657d1cd287595b29163e7ec3
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-12-12 12:53:35 -0800

    psuedo code for index.js file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const inquirer = require('inquirer');
 const { getById } = require('./lib/');
+
+// function startstory:
+// arrow function storyLine takes in an id
+// if id = 0, game over
+// else: getpromptsbyId(id)
+// store and destructure properties of prompts
+// return inquirer.prompt?
