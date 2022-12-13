3a284632b73fc9cea8071b4a48295ad531387428
Lex <106777728+Lexus-Banton@users.noreply.github.com>
2022-12-12 16:51:58 -0800

    worked on buliding functions for index

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
-const inquirer = require('inquirer');
+require('dotenv').config();
+//const inquirer = require('inquirer');
 const { getPromptById } = require('./lib/utils/utils.js');
+const sleep = (ms = 5000) => new Promise((r) => setTimeout(r, ms));
 
 // function startstory:
 async function startStory() {
   console.log('Welcome to What A Day!');
-  console.clear();
+  await sleep();
+  //console.clear();
+  return storyLine(1);
 }
 // arrow function storyLine takes in an id
-// if id = 0, game over
-// else: getpromptsbyId(id)
+const storyLine = async (id = 1) => {
+  // if id = 0, game over
+  if (id === 0) {
+    console.log('Thanks for playing!');
+    console.log(
+      'Developed By: Andrew Boyle, Emily Sellers, Lexus Banton, Morgan Niemeyer'
+    );
+  }
+  // else: getpromptsbyId(id)
+  const story = await getPromptById(id);
+  console.log('Potato', story);
+};
+
 // store and destructure properties of prompts
 // return inquirer.prompt?
+startStory();
